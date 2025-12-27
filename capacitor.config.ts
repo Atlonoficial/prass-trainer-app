428be0dd7149f0a1d534958d1a6cf739b5f572e6
Ebersção <eberscaowolff@gmail.com>
2025-12-27 12:47:00 -0300

    fix: Corrigir escala da tela - UIRequiresFullScreen e capacitor config - versao 17

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,23 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.prasstrainer.app',
   appName: 'Prass Trainer',
-  webDir: 'dist'
+  webDir: 'dist',
+  ios: {
+    contentInset: 'automatic',
+    allowsLinkPreview: false
+  },
+  server: {
+    iosScheme: 'capacitor',
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: true,
+      backgroundColor: '#000000',
+      showSpinner: false
+    }
+  }
 };
 
 export default config;
+
